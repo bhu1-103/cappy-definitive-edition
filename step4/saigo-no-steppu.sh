e14e39b1d25cd0450274f036adbde3b2cfed4012
bhu1-103 <103.bhuvan@gmail.com>
2024-11-05 23:36:30 +0530

    revert

--- step4/saigo-no-steppu.sh
+++ step4/saigo-no-steppu.sh
@@ -27,19 +27,19 @@ do
 	last_line=$((3+$AP))
     # Throughput (label 1)
     if [[ $line_id -eq 1 ]]; then
-		printf $line | paste -sd ',' >> $output_folder/throughput_$sce_id_$DEPLOYMENT_ID.csv
+		printf $line | paste -sd ',' >> $output_folder/throughput/throughput_$sce_id_$DEPLOYMENT_ID.csv
     # Airtime (label 2)    
     elif [[ $line_id -eq 2 ]]; then
-        printf $line | paste -sd ',' >> $output_folder/airtime_$sce_id_$DEPLOYMENT_ID.csv    
+        printf $line | paste -sd ',' >> $output_folder/airtime/airtime_$sce_id_$DEPLOYMENT_ID.csv    
     # RSSI list (feature 1)    
     elif [[ $line_id -eq 3 ]]; then    
-        printf $line | paste -sd ',' >> $output_folder/rssi_$sce_id_$DEPLOYMENT_ID.csv 
+        printf $line | paste -sd ',' >> $output_folder/rssi/rssi_$sce_id_$DEPLOYMENT_ID.csv 
     # Interference map (feature 2)   
     elif [[ $line_id -gt 3 && $line_id -le $last_line ]]; then
-		echo $line >> $output_folder/interference_$sce_id_$DEPLOYMENT_ID.csv
+		echo $line >> $output_folder/interference/interference_$sce_id_$DEPLOYMENT_ID.csv
     # Average SINR (feature 3)
 	elif [[ $line_id -eq $((4+$AP)) ]]; then
-		echo $line | paste -sd ',' >> $output_folder/sinr_$sce_id_$DEPLOYMENT_ID.csv 
+		echo $line | paste -sd ',' >> $output_folder/sinr/sinr_$sce_id_$DEPLOYMENT_ID.csv 
     fi
     line_id=$((line_id+1))
   fi
