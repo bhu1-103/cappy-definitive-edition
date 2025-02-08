281d811de4f6ac45c2ce8344d49ed6911958bf2e
bhu1-103 <103.bhuvan@gmail.com>
2025-02-08 18:30:17 +0530

    Sat Feb  8 06:30:17 PM IST 2025

--- create-folders.sh
+++ create-folders.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/zsh
 
-mkdir mkdir sce1a_output/
-mkdir mkdir sce1a_output/airtime
-mkdir mkdir sce1a_output/rssi
-mkdir mkdir sce1a_output/sinr
-mkdir mkdir sce1a_output/interference
-mkdir mkdir sce1a_output/throughput
+mkdir step4/sce1a_output/
+mkdir step4/sce1a_output/airtime
+mkdir step4/sce1a_output/rssi
+mkdir step4/sce1a_output/sinr
+mkdir step4/sce1a_output/interference
+mkdir step4/sce1a_output/throughput
