3927c65568b5ef4a9daceddc7067dc38f0a8c607
bhu1-103 <103.bhuvan@gmail.com>
2025-02-08 14:26:23 +0530

    Sat Feb  8 02:26:23 PM IST 2025

--- run.sh
+++ run.sh
@@ -7,7 +7,8 @@ alias blue='echo -e -n "\033[38;2;0;0;255m"'
 cd step1;
 #nvim AI_challenge_sce1.java; 
 nvim input-java.csv; ./step1.sh; red && echo -n "Step 1 done" && echo -e "\033[0m" ;cd ../
-cd step2; ./step2.sh; ./points.sh | wl-copy; pwd; ./v0.4/pls-work $(wl-paste)
+cd step2; ./step2.sh; 
+#./points.sh | wl-copy; pwd; ./v0.4/pls-work $(wl-paste)
 #xdotool key super+f; red && 
 echo -n "Step 2 done" && echo -e "\033[0m";cd ../
 cd step3; ./step3.sh; red && echo -n "Step 3 done" && echo -e "\033[0m";cd ../
