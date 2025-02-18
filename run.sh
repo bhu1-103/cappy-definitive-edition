5d4b0c251d0eceda286e3a56ca112ab48b2d54da
bhu1-103 <103.bhuvan@gmail.com>
2025-02-18 12:40:56 +0530

    Tue Feb 18 12:40:56 PM IST 2025

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ alias blue='echo -e -n "\033[38;2;0;0;255m"'
 ./create-folders.sh
 cd step1;
 #nvim AI_challenge_sce1.java;
-echo -n "enter your preferred text editor: "
-read EDITOR
+#echo -n "enter your preferred text editor: "
+#read EDITOR
 $EDITOR input-java.csv; ./step1.sh; red && echo -n "Step 1 done" && echo -e "\033[0m" ;cd ../
 cd step2; ./step2.sh; 
 #./points.sh | wl-copy; pwd; ./v0.4/pls-work $(wl-paste)
