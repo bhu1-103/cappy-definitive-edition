36fdabc272016ce5490c3d76307cc480a84bffc6
bhu1-103 <103.bhuvan@gmail.com>
2025-03-26 15:52:23 +0530

    padded in/*.csv to 2 digits

--- ML/pp.sh
+++ ML/pp.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/zsh
+
+rm -rf in; cp -r ../step2/z_output in
+rm -rf out; cp -r ../step4/sce1a_output/throughput out
+
 for file in in/*.csv
 do
     awk -F ";" '{
@@ -13,7 +17,7 @@ do
         }
         print prefix, number, $2, $4, $5, $6
     }' OFS=";" "$file" > temp && mv temp "$file"
-    cat $file | awk -F ";" '{print $1,$3,$4,$5,$6}' OFS=";" > temp && mv temp $file
+    cat $file | awk -F ";" '{printf "%s;%d;%0.2f;%0.2f;%0.2f\n",$1,$3,$4,$5,$6}' OFS=";" > temp && mv temp $file
     sed -i '1d' $file
 done
 
--- ML/pre-processing.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/zsh
-for file in in/*.csv
-do
-	cat $file | awk -F ";" '{print $1,$4,$5,$6}' OFS=";" > temp && mv temp $file
-done
