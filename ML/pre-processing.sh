8a23bcd44320d8937597ba85302e847f53779f7e
bhu1-103 <103.bhuvan@gmail.com>
2025-03-26 14:28:12 +0530

    Wed Mar 26 02:28:12 PM IST 2025

--- ML/pre-processing.sh
+++ ML/pre-processing.sh
@@ -2,5 +2,4 @@
 for file in in/*.csv
 do
 	cat $file | awk -F ";" '{print $1,$4,$5,$6}' OFS=";" > temp && mv temp $file
-	#cat $file | awk -F " " '{print $1";"$2";"$3";"$4}' > temp && mv temp $file
 done
